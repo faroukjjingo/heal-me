ebbaf4db1f4971ad0f53fbfd4c1e3773dc96514d
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-14 13:19:27 +0300

    Update chart.jsx

--- src/components/charts/chart.jsx
+++ src/components/charts/chart.jsx
@@ -2,7 +2,7 @@
 // src/components/ui/chart.jsx
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
-import { cn } from "@/lib/utils";
+import { cn } from ../../lib/utils";
 
 const ChartContainer = React.forwardRef(
   ({ config, className, children, ...props }, ref) => {
